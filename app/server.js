9804be9c723920351b18c93cb94fc4c65e504749
Tharindu Dilshan <patheex@gmail.com>
2026-04-27 17:56:51 +0530

    add health checks

--- app/server.js
+++ app/server.js
@@ -17,6 +17,21 @@ app.get("/", async (req, res) => {
   res.send(`<h1>Hello from Node + PostgreSQL</h1><p>DB time: ${result.rows[0].time}</p>`);
 });
 
+app.get("/health", async (req, res) => {
+  try {
+    await pool.query("SELECT 1");
+    res.status(200).json({
+      status: "ok",
+      database: "connected",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      database: "disconnected",
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
